e806ab349cc559132bc436b5cf364d77235631ba
ChiVo <kenv.aus@gmail.com>
2016-02-08 23:16:14 +1100

    okay

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
 
-var obj = {"IP Address" : null, "lang" : null};
+var obj = {"IP Address" : null, "lang" : null, "OS" : null};
 
 app.get("/", function(req,res) {
     obj["IP Address"] = req.header('x-forwarded-for') || req.connection.remoteAddress;
     obj["lang"] = req.headers["accept-language"].slice(0,2);
+    obj["OS"] = req.headers['user-agent'];
     console.log(obj);
     res.send(obj);
 });
